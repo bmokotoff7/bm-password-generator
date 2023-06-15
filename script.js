22379e3140b263ddeaa8c68200620818725930da
bmokotoff7 <116100468+bmokotoff7@users.noreply.github.com>
2023-06-15 18:31:53 -0400

    Select character group feature implemented

--- script.js
+++ script.js
@@ -3,22 +3,55 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
                     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-",
                     "+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+const uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+
+let uppercaseCheckboxEl = document.getElementById("uppercase-cb")
+let lowercaseCheckboxEl = document.getElementById("lowercase-cb")
+let numbersCheckboxEl = document.getElementById("numbers-cb")
+let symbolsCheckboxEl = document.getElementById("symbols-cb")
 let password1El = document.getElementById("password1")
 let password2El = document.getElementById("password2")
 
 let passwordLength = 15
 
 function generatePasswords() {
+    // determine available characters
+    let availableCharacters = []
+    if (uppercaseCheckboxEl.checked) {
+        for (let i = 0; i < uppercaseLetters.length; i++) {
+            availableCharacters += uppercaseLetters[i]
+        }
+    }
+    if (lowercaseCheckboxEl.checked) {
+        for (let i = 0; i < lowercaseLetters.length; i++) {
+            availableCharacters += lowercaseLetters[i]
+        }
+    }
+    if (numbersCheckboxEl.checked) {
+        for (let i = 0; i < numbers.length; i++) {
+            availableCharacters += numbers[i]
+        }
+    }
+    if (symbolsCheckboxEl.checked) {
+        for (let i = 0; i < symbols.length; i++) {
+            availableCharacters += symbols[i]
+        }
+    }
+    // generate both passwords
     let password1 = ""
     for (let i = 0; i <= passwordLength; i++) {
-        let randomInt = Math.floor(Math.random() * characters.length)
-        password1 += characters[randomInt]
+        let randomInt = Math.floor(Math.random() * availableCharacters.length)
+        password1 += availableCharacters[randomInt]
     }
     let password2 = ""
     for (let i = 0; i <= passwordLength; i++) {
-        let randomInt = Math.floor(Math.random() * characters.length)
-        password2 += characters[randomInt]
+        let randomInt = Math.floor(Math.random() * availableCharacters.length)
+        password2 += availableCharacters[randomInt]
     }
+    // display passwords to user
     password1El.textContent = password1
     password2El.textContent = password2
 }
