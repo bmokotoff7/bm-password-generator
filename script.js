56ff37b73f13fab6a1f1b3ab035e6331d6b2bae7
bmokotoff7 <116100468+bmokotoff7@users.noreply.github.com>
2023-06-16 13:53:55 -0400

    Styled password length slider

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-                    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
-                    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-",
-                    "+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
-
 const uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 const lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -13,6 +8,7 @@ let lowercaseCheckboxEl = document.getElementById("lowercase-cb")
 let numbersCheckboxEl = document.getElementById("numbers-cb")
 let symbolsCheckboxEl = document.getElementById("symbols-cb")
 let passwordLengthEl = document.getElementById("length-input")
+let passwordLengthSliderEl = document.getElementById("length-slider")
 let generatePasswordBtnEl = document.getElementById("generate-password-btn")
 let password1El = document.getElementById("password1")
 let password2El = document.getElementById("password2")
@@ -40,6 +36,8 @@ function generatePasswords() {
             availableCharacters += symbols[i]
         }
     }
+    // determine password length
+
     // generate both passwords
     if (availableCharacters.length > 0) {
         let password1 = ""
@@ -69,4 +67,14 @@ function styleGenerateBtn() {
     else {
         generatePasswordBtnEl.style.background = "#10B981"
     }
+}
+
+// Update password length in number input based on input from the slider
+function updateLength(newLength) {
+    passwordLengthEl.value = newLength
+}
+
+// Update length slider position based on inputs to the number input
+function updateSliderPosition(newPosition) {
+    passwordLengthSliderEl.value = newPosition
 }
